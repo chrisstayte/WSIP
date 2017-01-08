5ebace2553ad25eb38378cea048d52e1d4b0f922
Chris Stayte <ctstayte@gmail.com>
2017-01-08 13:28:39 -0500

    Add files via upload

---
+++ WSIP/Model/Project.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSIP.Model
+{
+    class Project : INotifyPropertyChanged
+    {
+        private string _name;
+        private int _numberOfGDB;
+        private double _size;
+        private string _projectPath;
+       
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // This method is called by the Set accessor of each property.
+        // The CallerMemberName attribute that is applied to the optional propertyName
+        // parameter causes the property name of the caller to be substituted as an argument.
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public Project(string name, string path)
+        {
+            _name = name;
+            _projectPath = path;
+            NumberOfGDB = 0;
+            Size = 0;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            } 
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        
+        public int NumberOfGDB
+        {
+            get
+            {
+                return _numberOfGDB;
+            } 
+            set
+            {
+                if (_numberOfGDB != value)
+                {
+                    _numberOfGDB = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        } 
+
+        public double Size
+        {
+            get
+            {
+                return _size;
+            }
+            set
+            {
+                if (_size != value)
+                {
+                    _size = value;
+                    NotifyPropertyChanged();
+                    NotifyPropertyChanged("SimpleSize");
+                }
+            }
+        }
+
+        public string ProjectPath
+        {
+            get
+            {
+                return _projectPath;
+            }
+        }
+
+        public string SimpleSize
+        {
+            get
+            {
+                if (_size == 0)
+                {
+                    return "Not Calculated";
+                }
+                double size = _size / 1048576.0;    // To MB
+                if (size < 1)
+                    return "Empty";
+                size = size / 1024.0;                // To GB
+                return Math.Round(size, 2).ToString();
+            }
+        }
+
+
+    }
+}
---
+++ WSIP/ViewModel/ViewModelMain.cs
@@ -0,0 +1,133 @@
+﻿using Ookii.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using WSIP.Helpers;
+using WSIP.Model;
+
+namespace WSIP.ViewModel
+{
+    class ViewModelMain : ViewModelBase
+    {
+        public ObservableCollection<Project> Projects { get; set; }
+        public ViewModelMain()
+        {
+            SelectProjectFolder = new RelayCommand(selectProjectFolder);
+            ProcessResults = new RelayCommand(processResults);
+            Projects = new ObservableCollection<Project>();
+        }
+
+        public RelayCommand SelectProjectFolder { get; private set; }
+        public RelayCommand ProcessResults { get; private set; }
+        public RelayCommand SortByName { get; private set; }
+
+        string _projectFolder;
+        public string ProjectFolder
+        {
+            get
+            {
+                return _projectFolder;
+            } 
+            set
+            {
+                if (_projectFolder != value)
+                {
+                    _projectFolder = value;
+                    RaisePropertyChanged("ProjectFolder");
+                }
+            }
+        }
+
+        private void selectProjectFolder(object parameter)
+        {
+            Debug.WriteLine("Parameter: " + (string)parameter);
+            Debug.WriteLine("Variable: " + _projectFolder);
+            VistaFolderBrowserDialog folderBrowserDialog = new VistaFolderBrowserDialog();
+            if (Directory.Exists((string)parameter))
+                folderBrowserDialog.SelectedPath = (string)parameter;
+
+            if (!VistaFolderBrowserDialog.IsVistaFolderDialogSupported)
+                System.Windows.MessageBox.Show("Because you are not using Windows Vista or later, the regular folder browser dialog will be used. Please use Windows Vista to see the new dialog.", "WSIP");
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                ProjectFolder = folderBrowserDialog.SelectedPath;
+            
+        }
+
+        private void processResults(object parameter)
+        {
+            if (!Directory.Exists(_projectFolder))
+            {
+                System.Windows.MessageBox.Show("Project Folder Doesn't Exist!", "WSIP", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            Projects.Clear();
+
+            List<string> projects = Directory.GetDirectories(_projectFolder).ToList();
+            foreach (string project in projects)
+            {
+                if (project != null)
+                {
+                    Projects.Add(new Model.Project(Path.GetFileName(project), project));
+                }
+            }
+
+            foreach (Project project in Projects)
+            {
+                Task.Run(() => getProjectSize(project));
+            }
+        }
+
+        private void getProjectSize(Project project)
+        {
+            getDirectorySize(project, project.ProjectPath);
+        }
+        private void getDirectorySize(Project project, string directoryPath)
+        {
+            string[] files = new String [0];
+            string[] subdirectorys = new String[0];
+
+            try
+            {
+                files = Directory.GetFiles(directoryPath);
+                subdirectorys = Directory.GetDirectories(directoryPath);
+            } catch (Exception ex)
+            {
+
+            }
+
+            foreach (String file in files)
+            {
+                getFileSize(project, file);
+            }
+            foreach (String subdirectory in subdirectorys)
+            {
+                if (Path.GetExtension(subdirectory) == ".gdb")
+                    project.NumberOfGDB += 1;
+                getDirectorySize(project, subdirectory);
+            }
+
+        }
+
+        private void getFileSize(Project project, string filePath)
+        {
+            try
+            {
+                FileInfo fi = new FileInfo(filePath);
+                project.Size += fi.Length;
+            } catch (Exception ex)
+            {
+
+            }
+        }
+
+    }
+}
