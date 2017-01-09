3f7ff24ac92dc4d597d19b876a81fc3c791aaf8e
WOOLPERTINC\Stayte <ctstayte@gmail.com>
2017-01-09 06:31:46 -0500

    Dll Compiled With Exe

--- WSIP/ViewModel/ViewModelMain.cs
+++ WSIP/ViewModel/ViewModelMain.cs
@@ -5,6 +5,7 @@ using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -23,6 +24,19 @@ namespace WSIP.ViewModel
             SelectProjectFolder = new RelayCommand(selectProjectFolder);
             ProcessResults = new RelayCommand(processResults);
             Projects = new ObservableCollection<Project>();
+
+            AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
+            {
+                string resourceName = new AssemblyName(args.Name).Name + ".dll";
+                string resource = Array.Find(this.GetType().Assembly.GetManifestResourceNames(), element => element.EndsWith(resourceName));
+
+                using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resource))
+                {
+                    Byte[] assemblyData = new Byte[stream.Length];
+                    stream.Read(assemblyData, 0, assemblyData.Length);
+                    return Assembly.Load(assemblyData);
+                }
+            };
         }
 
         public RelayCommand SelectProjectFolder { get; private set; }
